4419ab07a548168968c687572364a16d15694043
199999Y <eidanjunior.ariasp@gmail.com>
2025-01-20 20:15:44 -0500

    Add files via upload

---
+++ ConsoleApp2/Program1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Ingresa un número entero de dos dígitos: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        // Que el numero sea de dos digitos
+        if (numero >= 10 && numero <= 99 || numero <= -10 && numero >= -99)
+        {
+            // Datos
+            int unidad = Math.Abs(numero % 10);  // Último dígito
+            int decena = Math.Abs(numero / 10); // Primer dígito
+
+            // Suma
+            int suma = unidad + decena;
+
+            Console.WriteLine($"La suma de los dígitos de {numero} es: {suma}");
+        }
+        else
+        {
+            Console.WriteLine("El número ingresado no es de dos dígitos.");
+        }
+    }
+}
